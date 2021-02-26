6a64e39204ff93f58930f10ce2056f863aa3f5e1
Mario Hernandez <mariohernandez@MacBook-Air.lan1>
2021-02-25 23:39:41 -0600

    first commit

---
+++ src/main/java/com/tts/mycustomsite/MyCustomSiteApplication.java
@@ -0,0 +1,13 @@
+package com.tts.mycustomsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyCustomSiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyCustomSiteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tts/mycustomsite/MyCustomSiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tts.mycustomsite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyCustomSiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
